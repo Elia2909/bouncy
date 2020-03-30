a938d4389141db436a349dd7bdd05bbee534781b
Elia2909 <bulkaus@gmail.com>
2020-03-31 01:35:32 +0300

    Upd

--- path.js
+++ path.js
@@ -9,5 +9,19 @@ $('.biography-carousel').slick({
 $('.news-carusel').slick({
     dots:true,
     vertical:true,
-    arrows:false
+    arrows:false,
+    verticalSwiping:true
 });
+let burgerMenu = document.querySelector('.burger-menu'); // BURGER
+let topBarNavCol = document.querySelector('.header-menu'); // CONTAINER
+
+burgerMenu.addEventListener('click', ()=>{
+    if(!(topBarNavCol.classList.contains('active'))){
+        burgerMenu.classList.add('active');
+        topBarNavCol.classList.add('active');
+    }
+    else{
+        burgerMenu.classList.remove('active');
+        topBarNavCol.classList.remove('active');
+    }
+})
